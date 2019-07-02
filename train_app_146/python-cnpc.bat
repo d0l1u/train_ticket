607ddafa943990e658647bc9df74818d3a25c03e
meizs <meizs>
2019-07-02 19:15:17 +0800

    SPR-20180312

--- train_app_146/python-cnpc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set CATALINA_HOME=C:\data\train_app
-echo ******* python‘§∂® ROBOT-CHANGE 111*******
-cd %CATALINA_HOME%
-echo ******* python‘§∂® ROBOT-CHANGE 2222*******
-python cnpc.py
-exit
--- train_app_146/test_test.py
+++
@@ -1,6 +0,0 @@
-#-*-coding:utf8-*-
-import requests
-
-for i in range(100):
-    print requests.session().get(url="http://101.200.80.146:8000/async")
-    
